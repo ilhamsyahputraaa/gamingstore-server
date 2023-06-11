4c6200710b11e77ee1c275b0dbae001707e89e32
Ilhamsyah Putra <ilhamsyah.putraa1997@gmail.com>
2023-06-11 22:08:44 +0700

    API - Dashboard

--- app/API/router.js
+++ app/API/router.js
@@ -2,15 +2,21 @@ var express = require("express");
 var router = express.Router();
 const {
   landingPage,
-  detailPage, category,checkout,history,historyDetail
+  detailPage,
+  category,
+  checkout,
+  history,
+  historyDetail,
+  dashboard,
 } = require("./controller");
-const {isLoginPlayer} = require('../middleware/auth')
+const { isLoginPlayer } = require("../middleware/auth");
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
 router.get("/category", category);
-router.post("/checkout",isLoginPlayer, checkout);
-router.get("/history",isLoginPlayer, history);
-router.get("/history/:id/detail",isLoginPlayer, historyDetail);
+router.post("/checkout", isLoginPlayer, checkout);
+router.get("/history", isLoginPlayer, history);
+router.get("/history/:id/detail", isLoginPlayer, historyDetail);
+router.get("/dashboard", isLoginPlayer, dashboard);
 
 module.exports = router;
